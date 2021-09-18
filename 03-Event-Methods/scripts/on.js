1d4bda20033ff6dda6aa8ea318725c9699ebf96e
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-18 00:36:39 -0500

    Imp on EventMethod

---
+++ 03-Event-Methods/scripts/on.js
@@ -0,0 +1,18 @@
+/**
+ * What are Events?
+ * All the different visitors' actions that a web page can respond to are called events.
+ * An event represents the precise moment when something happens.
+ * 
+ * jQuery selects elements the same way CSS does: with selectors.
+ * Event methods trigger or attach a function to an event handler for the selected elements.
+ * 
+ * jQuery focus() Method
+ * The focus() method attaches an event handler function to an HTML element.
+ * The function is executed when the form field gets focus:
+ * 
+ */
+$(document).ready(function() {
+    $("p").on("click", function() {
+        $(this).hide();
+    });
+});
