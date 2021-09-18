367bb5bd1824cc3629ee7e32fdbed2548a956637
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-18 00:36:01 -0500

    Imp multiple-event-handlers

---
+++ 03-Event-Methods/scripts/multiple-event-handlers.js
@@ -0,0 +1,24 @@
+/**
+ * What are Events?
+ * All the different visitors' actions that a web page can respond to are called events.
+ * An event represents the precise moment when something happens.
+ * 
+ * jQuery selects elements the same way CSS does: with selectors.
+ * Event methods trigger or attach a function to an event handler for the selected elements.
+ * 
+ * Attach multiple event handlers to a <p> element:
+ * 
+ */
+$(document).ready(function() {
+    $("p").on({
+        mouseenter: function() {
+            $(this).css("background-color", "lightgray");
+        },
+        mouseleave: function() {
+            $(this).css("background-color", "lightblue");
+        },
+        click: function() {
+            $(this).css("background-color", "yellow");
+        }
+    });
+});
